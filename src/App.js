0610fed5c7c0cc60285ce814561f9608cb7b88b2
nhash46 <61870054+nhash46@users.noreply.github.com>
2020-07-06 22:24:44 +1000

    created AddTodo button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Todos from './components/Todos';
 import Header from './components/layout/Header'
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 import './App.css';
 
@@ -48,8 +49,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Header/>
-        <Todos todos= {this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+        <div className="container">
+          <Header/>
+          <AddTodo/>
+          <Todos todos= {this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+        </div>
       </div>
     );
   }
