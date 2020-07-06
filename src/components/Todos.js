54ffa694f566a6826bcce9508cf8494d5637b9fd
nhash46 <61870054+nhash46@users.noreply.github.com>
2020-07-06 16:15:56 +1000

    clean up

---
+++ src/components/Todos.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+
+class Todos extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return this.props.todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo}/>
+    ));
+  }
+}
+
+export default Todos;
