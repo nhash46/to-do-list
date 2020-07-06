08df95ab30f8842d3b2a6a165ae3789bcdaeb881
nhash46 <61870054+nhash46@users.noreply.github.com>
2020-07-06 22:05:51 +1000

    updated Header file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Todos from './components/Todos';
+import Header from './components/layout/Header'
 
 import './App.css';
 
@@ -47,6 +48,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Header/>
         <Todos todos= {this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
       </div>
     );
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,10 +2,17 @@ import React from 'react'
 
 function Header() {
     return (
-        <div>
-            <h1>TodoList</h1>
-        </div>
+        <header style={headerStyle}>
+            <h1>To do List</h1>
+        </header>
     )
+}
+
+const headerStyle = {
+    background: '#333',
+    color: '#fff',
+    textAlign: 'center',
+    padding: '10px'
 }
 
 export default Header;
