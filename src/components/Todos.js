0e6be2d77baaceabfa7e33f099e073717f1cbe66
nhash46 <61870054+nhash46@users.noreply.github.com>
2020-07-06 16:29:33 +1000

    props

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
 
 class Todos extends React.Component {
   constructor(props) {
@@ -11,6 +12,11 @@ class Todos extends React.Component {
         <TodoItem key={todo.id} todo={todo}/>
     ));
   }
+}
+
+// PropTypes
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired
 }
 
 export default Todos;
